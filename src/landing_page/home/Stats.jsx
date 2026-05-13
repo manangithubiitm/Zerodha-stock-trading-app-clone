3afbba7f739d369b8f22c382ed154a1d46056bf3
Manan Tilwani <21f2000858@ds.study.iitm.ac.in>
2026-05-13 21:31:52 +0530

    Made home component for Zerodha Project

---
+++ src/landing_page/home/Stats.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Stats() {
+    return ( 
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Trust with confidence</h1>
+                </div>
+                <div className="col-6">
+                    <img src="media/images/ecosystem.png" style={{width:"75%"}} />
+                </div>
+
+            </div>
+        </div>
+     );
+}
+
+export default Stats;
